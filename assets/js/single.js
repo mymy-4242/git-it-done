4ec39b4466cce3f1672c9291a3302460c6b28409
Local MM <73925149+mymy-4242@users.noreply.github.com>
2021-01-14 18:49:54 -0800

    add display issues function

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,41 @@
+var displayIssues = function(issues) {
+    for(var i=0; i < issues.length; i++) {
+        //create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        //create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        //append to container
+        issueEl.appendChild(titleEl);
+
+        //create a type element
+        var typeEl = document.createElement("span");
+
+        //check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        //append to container
+        issueEl.appendChild(typeEl);
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(Response) {
         //request was successful
         if(Response.ok) {
             Response.json().then(function(data) {
-                console.log(data);
+                //pass response data to dom function
+                displayIssues();
             });
         }
         else {
@@ -12,5 +43,7 @@ var getRepoIssues = function(repo) {
         }
     });
 };
+
+
 
 getRepoIssues("facebook/react");
