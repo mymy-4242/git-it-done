f00050199feced7a5c09c623fef216b5a1372d02
Local MM <73925149+mymy-4242@users.noreply.github.com>
2021-01-16 17:33:17 -0800

    added search buttons for different languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,20 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
 
 var getUserRepos = function(user) {
     //format the github api url
@@ -92,3 +107,16 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
